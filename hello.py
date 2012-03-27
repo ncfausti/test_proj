1a99a7b7fdf6b0d511f8b0d8d18ee823711cf12b
Yarkee Chou <iyarkee@gmail.com>
2012-03-27 11:03:15 +0800

    First commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+#! /usr/bin/eny python
+# -*-coding:utf8-*-
+
+print 'hello world'
+print 'hello world again'
+print 'hello world once time'
+
+def say_hello():
+    'This func is used to output hello and world'
+    print 'hello'
+    print 'world'
+
+if __name__ == '__main__':
+    say_hello()
+
